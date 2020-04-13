5188dd20598df4c74450c947a61e5e4e20253537
pmcwhannel <pmcw911@gmail.com>
2020-04-12 21:40:07 -0400

    Updated files, RSNN added, save plots add to gen data

---
+++ Class_generate_data.py
@@ -0,0 +1,115 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+class generate_data():
+
+    def __init__(self, mu, std, range_cov, range_coef, range_bias):
+        '''
+        generator = generate_data(mu, std, range_cov, range_coef, range_bias)
+
+        Creates a data generator, for all your data generating needs.
+
+        Inputs:
+         mu: Mean of the noise.
+         std: Standard deviation of the noise..
+         range_cov: Size of interval for uniform distribution to generate covariates of the data.
+         range_coef: Size of interval for uniform distribution to generate coefficients of gen process.
+         range_bias: Size of interval for uniform distribution to generate bias of gen process.
+        '''
+
+        # Parameters for generating data
+        self.mu = mu
+        self.std = std
+        self.rng = range_cov
+        self.rng_coef = range_coef
+        self.rng_bias = range_bias
+
+    def generate(self, n, p, seed=100):
+        '''
+
+        x_data, noisy_labels, true_labels = generator.generate(n, p, seed)
+
+        Crates data
+
+        Inputs:
+          n: The number of data points requested. (scalar)
+          p: The number of features. (scalar)
+          seed: Seed for random generation, can be used to reproduce results. (Integer)
+        Outputs:
+          Returns 3 items.
+          The first item returned are covariates of the data as an np.array with shape (n,p).
+          The second item returned is the labels generated with noise with shape (n,1).
+          The third item returned is the labels generated without nouse with shape (n,1).
+
+        '''
+        # Sets seed for generation of data.
+        np.random.seed(seed)
+
+        self.n = n  # Store the number of samples requested.
+        self.p = p  # Store the number of covariates.
+
+        # Parameters of generating process.
+        self.coef = np.random.uniform(size=p, low=-self.rng_coef,
+                                      high=self.rng_coef)  # The coefficients for each co-varitate in the data generating process. (1D np.array of shape (p,))
+        self.bias = np.random.uniform(size=1, low=-self.rng_bias,
+                                      high=self.rng_bias)  # The RHS threshold in the data generating process. (scalar)
+
+        # Generating Process.
+        eps = np.random.normal(loc=self.mu, scale=self.std, size=n)  # Creates noise to be used in generating y-labels.
+        self.x = np.random.uniform(size=(n, p), low=-self.rng, high=self.rng)  # Creates covariates.
+        self.y_true = (np.dot(self.x, self.coef) > self.bias) * np.ones((n))  # Creates y_true, labels without noise.
+        self.y = (np.dot(self.x, self.coef) + eps > self.bias) * np.ones((n))  # Creates labels.
+
+        return (self.x, np.reshape((self.y), (n, 1)), np.reshape((self.y_true), (n, 1)))
+
+    def plot(self, truth=False):
+        '''
+
+        plot= Weight(n, p, coef, bias, seed, truth)
+
+        Plots the space of the covariates and shows the decision boundary with their true labels.
+
+        Inputs:
+          truth: Whether to label using true or noisy labels. (boolean)
+
+        '''
+
+        if (truth == 1):
+            x_zero = self.x[np.argwhere(self.y_true == 1)[:, 0].tolist()]  # Points of label 1.
+            x_one = self.x[np.argwhere(self.y_true == 0)[:, 0].tolist()]  # Points of Label 0.
+            title = 'True Labels'
+        else:
+            x_zero = self.x[np.argwhere(self.y == 1)[:, 0].tolist()]  # Points of label 1.
+            x_one = self.x[np.argwhere(self.y == 0)[:, 0].tolist()]  # Points of Label 0.
+            title = 'Noisy Labels'
+
+        if (self.p == 1):  # Plot for when only 1 covariate
+            plt.figure(100)
+            plt.xlabel('Dimension 1')
+            plt.title('Labelled data in covariate space' + '(' + title + ')')
+            plt.plot([self.bias / self.coef] * 3, [0.5, 1, 1.5], c='black')
+            plt.scatter(x_zero, np.ones(len(x_zero)), c='r')
+            plt.scatter(x_one, np.ones(len(x_one)), c='b')
+            plt.legend(['Decision Boundary', 'Label 0', 'Label 1'])
+            plt.savefig('1D_generated_points.png')
+            plt.show()
+
+
+        elif (self.p == 2):  # Plot for when 2 covariates are available.
+            plt.figure(100).savefig('2D_generated_points.png')
+            plt.ylabel('Dimension 2')
+            plt.xlabel('Dimension 1')
+            plt.title('Labelled data in covariate space,' + '(' + title + ')')
+            x_temp = np.linspace(start=-self.rng, stop=self.rng, num=10)  # Points to plot decision boundary.
+            y_temp = (self.bias - self.coef[0] * x_temp) / self.coef[1]
+            plt.plot(x_temp, y_temp, c='black')
+            plt.scatter(x_zero[:, 0], x_zero[:, 1], c='r')
+            plt.scatter(x_one[:, 0], x_one[:, 1], c='b')
+            plt.legend(['Decision Boundary', 'Label 0', 'Label 1'])
+            plt.savefig('2D_generated_points.png')
+
+            plt.show()
+
+        else:
+            print('Sorry, not complete for more than 2 dimensions.')
--- network_benchmark_plots.py
+++ network_benchmark_plots.py
@@ -1,7 +1,11 @@
+import sys
+sys.path.insert(0,'C:\\Users\\pmcw9\\Winter 2020\\CS 698\\Project Final Folder\\network files')
 import Class_Network as Network
+import Class_generate_data as generate_data
 from sklearn.model_selection import KFold
 import numpy as np
 import pandas as pd
+import matplotlib.pyplot as plt
 from keras.models import Sequential
 from keras.layers import Dense
 from keras import optimizers
@@ -9,20 +13,21 @@ from keras import regularizers
 import configparser
 
 
-def keras_test(X_train, X_test, y_train, y_test, h_nodes, epochs, batch_size, lr, weight_decay):
+def keras_test(X_train, X_test, y_train, y_test, h_nodes, epochs, batch_size, lr, weight_decay, type):
     """
-    Trains a regular 3-layer neural net in keras, and returns the accuracy results
+    Trains a regular 3-layer neural net in keras, and returns the accuracy results.
 
-    :param X_train: numpy array/pandas dataframe, data for the keras model
-    :param X_test: numpy array/pandas dataframe, data for the keras model
-    :param y_train: numpy array/pandas dataframe, data for the keras model
-    :param y_test: numpy array/pandas dataframe, data for the keras model
-    :param h_nodes: int, number of nodes in the hidden layer
-    :param epochs: int, number of epochs to train the NN
-    :param batch_size: int, size of a batch when training
-    :param lr: float, learning rate for training
-    :param weight_decay: float, l2-norm weight decay constant
-    :return: (training_acc, test_acc), lists of training/testing accuracies
+    :param X_train: numpy array/pandas dataframe, data for the keras model.
+    :param X_test: numpy array/pandas dataframe, data for the keras model.
+    :param y_train: numpy array/pandas dataframe, data for the keras model.
+    :param y_test: numpy array/pandas dataframe, data for the keras model.
+    :param h_nodes: int, number of nodes in the hidden layer.
+    :param epochs: int, number of epochs to train the NN.
+    :param batch_size: int, size of a batch when training.
+    :param lr: float, learning rate for training.
+    :param weight_decay: float, l2-norm weight decay constant.
+    :param type: string, select type of loss function.
+    :return: (training_acc, test_acc), lists of training/testing accuracies.
     """
 
     in_dim = X_train.shape[1]
@@ -35,12 +40,44 @@ def keras_test(X_train, X_test, y_train, y_test, h_nodes, epochs, batch_size, lr
                           kernel_regularizer=regularizers.l2(weight_decay)))
 
     optimizer = optimizers.SGD(lr=lr)
-    keras_model.compile(loss='categorical_crossentropy', optimizer=optimizer, metrics=['accuracy'])
+    keras_model.compile(loss=type, optimizer=optimizer, metrics=['accuracy'])
 
     history = keras_model.fit(X_train, y_train, epochs=epochs, batch_size=batch_size,
                               validation_data=(X_test, y_test))
 
-    return (history.history['acc'], history.history['val_acc'])
+    return (history.history['accuracy'], history.history['val_accuracy'])
+
+
+def RSNN(X_train, X_test, y_train, y_test, h_nodes, epochs, lr, times, threshold, coefficient, type):
+    """
+    Trains a regular 3-layer our NN, and returns the accuracy results
+
+    :param X_train: Numpy array/pandas dataframe, data for the keras model.
+    :param X_test: Numpy array/pandas dataframe, data for the keras model.
+    :param y_train: Numpy array/pandas dataframe, data for the keras model.
+    :param y_test: Numpy array/pandas dataframe, data for the keras model.
+    :param h_nodes: Int, number of nodes in the hidden layer.
+    :param epochs: Int, number of epochs to train the NN.
+    :param lr: Float, learning rate for training.
+    :param times: Integer, number of times to MC sample.
+    :param threshold: Integer, used as threshold for forming residual to sample from.
+    :param coefficient: Float, used as the ratio for updating the variance.
+    :param type: string, select type of loss function.
+    :return: (training_acc, test_acc), lists of training/testing accuracies
+    """
+    in_dim = X_train.shape[1]
+    out_dim = y_train.shape[1]
+
+    #Initialize the network
+    net = Network.Network([in_dim, h_nodes, out_dim], type = type, pdw = ['gaussian']*2, pdb = ['gaussian']*2)
+    acc_train = [0]*epochs # For storing training accuracy.
+    acc_test = [0]*epochs # For storing test accuracy.
+    for i in range(0, epochs):
+        net.Learn(X_train, y_train, epochs=1, lrate = lr, times = times, threshold = threshold, bootstrap = False, coefficient = coefficient)
+        acc_train[i] = net.ClassificationAccuracy(X_train, y_train)
+        acc_test[i] = net.ClassificationAccuracy(X_test, y_test)
+
+    return(acc_train, acc_test)
 
 
 def network_benchmark_plots(X_train, X_test, y_train, y_test, rs_nn_params, keras_params, decay_params):
@@ -63,18 +100,42 @@ def network_benchmark_plots(X_train, X_test, y_train, y_test, rs_nn_params, kera
     :return:
     """
     # Train a RS-NN
+    (RSNN_train_acc, RSNN_test_acc) = RSNN(X_train, X_test, y_train, y_test, int(rs_nn_params['h_nodes']), int(rs_nn_params['epochs']), float(rs_nn_params['lr']), int(rs_nn_params['times']),float(rs_nn_params['threshold']), float(rs_nn_params['coefficient']),type= rs_nn_params['type'])
 
     # Train a regular NN
     (keras_train_acc, keras_test_acc) = keras_test(X_train, X_test, y_train, y_test, int(keras_params['h_nodes']),
                                                    int(keras_params['epochs']), int(keras_params['batch_size']),
-                                                   float(keras_params['lr']), weight_decay=0)
+                                                   float(keras_params['lr']), weight_decay=0,type= keras_params['type'])
 
     # Train a regular NN with weight decay
-    (decay_train_acc, decay_test_acc) = keras_test(X_train, X_test, y_train, y_test, int(decay_params['decay']['h_nodes']),
+    (decay_train_acc, decay_test_acc) = keras_test(X_train, X_test, y_train, y_test, int(decay_params['h_nodes']),
                                                    int(decay_params['epochs']), int(decay_params['batch_size']),
-                                                   float(decay_params['lr']), float(decay_params['weight_decay']))
+                                                   float(decay_params['lr']), float(decay_params['weight_decay']),type=decay_params['type'])
 
-    # plot the accuracies
+    # Plot the accuracies
+    rsnn_eps = np.arange(1, int(rs_nn_params['epochs']) + 1)
+    keras_eps = np.arange(1, int(keras_params['epochs']) + 1)
+    decay_eps = np.arange(1, int(decay_params['epochs']) + 1)
+
+    plt.figure(0)
+    plt.title("Training Accuracy vs. Epochs)")
+    plt.plot(rsnn_eps, RSNN_train_acc)
+    plt.plot(keras_eps, keras_train_acc)
+    plt.plot(decay_eps,decay_train_acc)
+    plt.legend(['RSNN', 'Normal NN','NN with decay'])
+    plt.xlabel('Epochs')
+    plt.ylabel('Accuracy')
+    plt.show()
+
+    plt.figure(1)
+    plt.title("Test Accuracy vs. Epochs)")
+    plt.plot(rsnn_eps, RSNN_test_acc)
+    plt.plot(keras_eps, keras_test_acc)
+    plt.plot(decay_eps, decay_test_acc)
+    plt.legend(['RSNN', 'Normal NN', 'NN with decay'])
+    plt.xlabel('Epochs')
+    plt.ylabel('Accuracy')
+    plt.show()
 
 
 def main():
@@ -82,13 +143,17 @@ def main():
     config.read('config.ini')
 
     # select correct data
-    train_size = int(config['data']['train_size'])
-    test_size = int(config['data']['test_size'])
+    train_size = int(config['DATA']['train_size'])
+    test_size = int(config['DATA']['test_size'])
 
-    if config['data']['dataset'] == "simulated":
-        X_train, X_test, y_train, y_test = []
+    if config['DATA']['dataset'] == "simulated":
+        generator = generate_data.generate_data(0, 3, 5, 2, 2.5) # Maybe add to config file..
+        num_covariates = 2 # number of covariates
+        X_train, y_train, yt = generator.generate(n=train_size, p=num_covariates)
+        X_test,y_test,yt = generator.generate(n=test_size, p=num_covariates)
 
-    if config['data']['dataset'] == "mnist":
+
+    if config['DATA']['dataset'] == "mnist":
         import mnist_loader
         train_full, validate_full, test_full = mnist_loader.load_data_wrapper() # we wont need validate dataset
         X_train = np.array(train_full[0][:train_size])
@@ -97,8 +162,8 @@ def main():
         y_test = np.array(test_full[1][:test_size])
 
     # run benchmarking function
-    network_benchmark_plots(X_train, X_test, y_train, y_test, config['RS NN PARAMS'], config['KERAS PARAMS'], config['DECAY PARAMS'])
-
+    network_benchmark_plots(X_train, X_test, y_train, y_test, config['RS NN PARAMS'], config['KERAS PARAMS'],
+                            config['DECAY PARAMS'])
 
 if __name__ == "__main__":
     main()
