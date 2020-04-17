9b3066a4ebaf8b0ce626c77a32cdcb6e39f244c6
Vincent <v3luong@uwaterloo.ca>
2020-04-16 20:10:45 -0400

    added packaging files

---
+++ setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="residual-sample-nn",
+    version="0.0.1",
+    author="Ren Yuan Xue, Pierre McWhannel, Vincent Luong",
+    author_email="v3luong@uwaterloo.ca",
+    description="An implementation of a residual sample neural net",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/vinceLuong/residual-sample-nn/",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
